5379f9b396d44b863c99c6748f2a83738e62e4dd
Ekevoo <x@ekevoo.com>
2018-11-07 10:01:25 -0500

    Pulling using "bestaudio", though it could be any format.

--- main.py
+++ main.py
@@ -11,14 +11,15 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 #
-from logging import getLogger
+import logging
+import time
 from os.path import exists, dirname, join
-from time import sleep
 
-from git import Repo
-from pyperclip import paste
+import git
+import pyperclip
 
-log = getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+log = logging.getLogger(__name__)
 repository_url = 'https://github.com/rg3/youtube-dl.git'
 base_path = dirname(__file__)
 
@@ -29,45 +30,45 @@ def ensure_ydl():
     # Check if it exists
     if not exists(repo_path):
         log.warning('Youtube-dl not found! Dowloading from %s', repository_url)
-        Repo.clone_from(repository_url, repo_path)
+        git.Repo.clone_from(repository_url, repo_path)
 
     # Check if it's old
     else:
-        remote = Repo(repo_path).remote()
-        log.info('Updating from %s', remote.name)
+        remote = git.Repo(repo_path).remote()
+        log.info('Updating downloader from %s', remote.name)
         remote.pull()
 
     from ydl.youtube_dl import YoutubeDL
     return YoutubeDL
 
 
-def pull(items):
-    YoutubeDL = ensure_ydl()
+def pull(downloader, items):
     ydl_options = {
         'ignoreerrors': True,
-        'format': 251,  # DASH audio,141k, opus @160k, 5.90MiB
+        'format': 'bestaudio',
         'outtmpl': join(base_path, '%(title)s %(uploader)s-%(id)s.%(ext)s'),
     }
-    with YoutubeDL(ydl_options) as youtube_dl:
+    with downloader(ydl_options) as youtube_dl:
         youtube_dl.download(items)
 
 
 def main():
-    clipboard = [i for i in (paste() or '').split() if i]
-    print('------------------------------------------------------------------------')
-    print('')
+    clipboard = [i for i in (pyperclip.paste() or '').split() if i]
+    log.info('------------------------------------------------------------------------')
+    downloader = ensure_ydl()
+    log.info('')
     if clipboard:
-        print('Clipboard contents:')
+        log.info('Clipboard contents:')
         for i in clipboard:
-            print('-', i)
-        pull(clipboard)
+            log.info('- %s', i)
+        pull(downloader, clipboard)
     else:
-        print('Nothing on the clipboard.')
-    print('')
-    print('------------------------------------------------------------------------')
-    print('')
-    print('Done!')
-    sleep(30)
+        log.info('Nothing on the clipboard.')
+    log.info('')
+    log.info('------------------------------------------------------------------------')
+    log.info('')
+    log.info('Done!')
+    time.sleep(30)
 
 
 if __name__ == '__main__':
