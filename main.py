cd05f3a7e0a8330711a7ab550792716cd5574962
Ekevoo <x@ekevoo.com>
2018-11-21 17:24:14 -0500

    Laying some groundwork for post-processing.

--- main.py
+++ main.py
@@ -12,8 +12,10 @@
 # See the License for the specific language governing permissions and limitations under the License.
 #
 import logging
+import os
+import sys
 import time
-from os.path import exists, dirname, join
+from pathlib import Path
 
 import git
 import pyperclip
@@ -21,35 +23,55 @@ import pyperclip
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 log = logging.getLogger(__name__)
 repository_url = 'https://github.com/rg3/youtube-dl.git'
-base_path = dirname(__file__)
+base_path = Path(__file__).resolve().parent
 
 
 def ensure_ydl():
-    repo_path = join(base_path, 'ydl')
+    repo_path = base_path.joinpath('ydl')
 
     # Check if it exists
-    if not exists(repo_path):
+    if not repo_path.exists():
         log.warning('Youtube-dl not found! Dowloading from %s', repository_url)
-        git.Repo.clone_from(repository_url, repo_path)
+        log.warning('This first run takes several minutes. Please distract yourself with something else.')
+        git.Repo.clone_from(repository_url, str(repo_path))
 
     # Check if it's old
     else:
-        remote = git.Repo(repo_path).remote()
-        log.info('Updating downloader from %s', remote.name)
-        remote.pull()
+        repo = git.Repo(str(repo_path))
+        if repo.is_dirty():
+            log.warning('Custom changes detected in Youtube-dl! Not checking for updates.')
+        else:
+            remote = repo.remote()
+            log.info('Checking for Youtube-dl updates from %s. This should only take a few seconds.', remote.name)
+            remote.pull()
 
+    sys.path.append(str(base_path))
     from ydl.youtube_dl import YoutubeDL
     return YoutubeDL
+
+
+class Converter:
+    _converter_path = None
+
+    def check(self, filename: str):
+        target_path = Path(filename)
+        if target_path.suffix == '.mp3':
+            return
+
+        log.info(target_path.with_suffix('.mp3'))
 
 
 def pull(downloader, items):
     ydl_options = {
         'ignoreerrors': True,
         'format': 'bestaudio',
-        'outtmpl': join(base_path, '%(title)s %(uploader)s-%(id)s.%(ext)s'),
+        'outtmpl': os.getcwd() + '/%(title)s %(uploader)s-%(id)s.%(ext)s',
     }
+    converter = Converter()
     with downloader(ydl_options) as youtube_dl:
-        youtube_dl.download(items)
+        for item in items:
+            result = youtube_dl.extract_info(item, process=True)
+            converter.check(result['filename'])
 
 
 def main():
